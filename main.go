8603a6c37cb1fbdee7e4025dddd1405253e1c8e1
Jimmeh <jimmeh.mcdonald@gmail.com>
2024-03-16 18:47:08 +0000

    More refactoring

--- main.go
+++ main.go
@@ -3,49 +3,12 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/fs"
 	"os"
+
+	"github.com/Jimmeh/jim-ls/jls"
 )
 
-type myFS struct {
-	pwd     func() (string, error)
-	listDir func(string) ([]fs.DirEntry, error)
-}
-
-var filesystem = myFS{
-	pwd:     os.Getwd,
-	listDir: os.ReadDir,
-}
-
-type DirectoryListing struct {
-	entries []fs.DirEntry
-}
-
-func (listing *DirectoryListing) Print(includeHidden bool, disp Display) {
-	for _, entry := range listing.entries {
-		entryLine := entry.Name()
-		if includeHidden || entryLine[0] != '.' {
-			disp.Show(entry.Name(), entry.IsDir())
-		}
-	}
-}
-
-func NewDirectoryListing() (DirectoryListing, error) {
-	dir, err := filesystem.pwd()
-	if err != nil {
-		return DirectoryListing{}, fmt.Errorf("unable to get current directory: %s", err)
-	}
-
-	listing, err := filesystem.listDir(dir)
-	if err != nil {
-		return DirectoryListing{}, fmt.Errorf("unable to get contents of current directory: %s", err)
-	}
-	return DirectoryListing{listing}, nil
-}
-
-type Display interface {
-	Show(item string, highlight bool)
-}
+var filesystem = jls.NewFS(os.Getwd, os.ReadDir)
 
 type ConsoleDisplay struct {
 }
@@ -62,7 +25,10 @@ func main() {
 	all := flag.Bool("a", false, "include hidden files/folders")
 	flag.Parse()
 
-	listing, err := NewDirectoryListing()
+	opts := jls.ListingOptions{
+		IncludeHidden: *all,
+	}
+	listing, err := jls.NewDirectoryListing(filesystem, opts)
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -70,6 +36,6 @@ func main() {
 
 	display := ConsoleDisplay{}
 
-	listing.Print(*all, display)
+	listing.Show(*all, display)
 	fmt.Println()
 }
